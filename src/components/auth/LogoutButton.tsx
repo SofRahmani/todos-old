b6ea91e4cb82d6d5e17ca01b92fb4feba26115ef
SofRahmani <sofiane.rahmani12@gmail.com>
2024-05-07 20:26:11 +0200

    style: :lipstick: logout button

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,16 +1,19 @@
 "use client"; 
 import { signOutAction } from "@/actions/auth.action";
 import { Button } from "@/components/ui/button";
+import { LogOutIcon } from 'lucide-react'
 
 export default function LogoutButton() {
   return (
     <Button
-      variant={"default"}
+      variant={"link"}
+      className='text-red-400'
       onClick={() => {
         signOutAction();
       }}
     >
       Déconnexion
+      <LogOutIcon className='ml-4' />
     </Button>
   );
 }
